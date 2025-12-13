6c303ff7a52ad62450b195108ff1ffc5d9815543
Grenish rai <mrcoder2033d@gmail.com>
2025-12-14 01:17:37 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
-import { ComponentExample } from "@/components/component-example";
+import Hero from "@/components/hero";
 
 export default function Page() {
-return <ComponentExample />;
+  return (
+    <>
+      <Hero />
+    </>
+  );
 }
