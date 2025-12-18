47405948d77345a67d2946a425abc48b69630357
Grenish rai <mrcoder2033d@gmail.com>
2025-12-19 02:08:45 +0530

    Add background, overlay and uppercase logo

--- app/(auth)/[lang]/login/page.tsx
+++ app/(auth)/[lang]/login/page.tsx
@@ -13,11 +13,12 @@ export default async function LoginPage({
   if (!t) return notFound();
 
   return (
-    <div className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <div className="flex w-full max-w-sm flex-col gap-6">
+    <div className="bg-smooth bg-cover bg-no-repeat bg-center flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10 relative">
+      <div className="absolute inset-0 backdrop-blur-xs bg-muted/50" />
+      <div className="flex w-full max-w-sm flex-col gap-6 relative">
         <a href="#" className="flex items-center gap-2 self-center font-medium">
           <div className="text-primary-foreground flex items-center justify-center">
-            <h2>Kairo</h2>
+            <h2 className="font-bold tracking-widest">KAIRO</h2>
           </div>
         </a>
         <LoginForm t={t} />
