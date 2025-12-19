5c78bdbcfc5c8e51cf57f8840791ef06e4e17b9f
Grenish rai <mrcoder2033d@gmail.com>
2025-12-19 16:33:45 +0530

    Link login logo to locale and pass lang prop

--- app/(auth)/[lang]/login/page.tsx
+++ app/(auth)/[lang]/login/page.tsx
@@ -16,12 +16,15 @@ export default async function LoginPage({
     <div className="bg-smooth bg-cover bg-no-repeat bg-center flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10 relative">
       <div className="absolute inset-0 backdrop-blur-xs bg-muted/50" />
       <div className="flex w-full max-w-sm flex-col gap-6 relative">
-        <a href="#" className="flex items-center gap-2 self-center font-medium">
+        <a
+          href={`/${lang}`}
+          className="flex items-center gap-2 self-center font-medium"
+        >
           <div className="text-primary-foreground flex items-center justify-center">
             <h2 className="font-bold tracking-widest">KAIRO</h2>
           </div>
         </a>
-        <LoginForm t={t} />
+        <LoginForm t={t} lang={lang} />
       </div>
     </div>
   );
